d4bec02de42bee9e3abf79e4373b91bf674db46d
manish0870 <manishchoudhary0870@gmail.com>
2019-10-08 14:08:56 +0530

    Map & Responsiveness added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 // Initialize and add the map
-function initMap() {
+L.mapquest.key = 'wRxlTgJSKje6C9Q5esMeYfz1HRLUaQ0X';
+
+// 'map' refers to a <div> element with the ID map
+const map = L.mapquest.map('map', {
+    center: [12.979599, 77.591566],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 13,
+    scrollWheelZoom: false,
+    draggable: true
+});
+
+L.marker([12.979599, 77.591566], {
+    draggable: false,
+    alt: 'Marker',
+    zIndexOffset: 0,
+    opacity: 0.8,
+    title: "That's Us",
+    riseOnHover: true
+}).addTo(map);
+
+/* function initMap() {
     // Your location
     const loc = { lat: 12.971599, lng: 77.594566 };
     // Centered map on location
@@ -9,7 +29,7 @@ function initMap() {
     });
     // The marker, positioned at location
     const marker = new google.maps.Marker({ position: loc, map: map });
-}
+} */
 
 // Sticky menu background
 window.addEventListener('scroll', function() {
